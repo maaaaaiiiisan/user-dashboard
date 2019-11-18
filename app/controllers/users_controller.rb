0e2da58a8d177b660c74d18bcddc557b25e2be3b
maaaaaiiiisan <m.fukuzumi@reasonwhy.jp>
2019-11-18 21:10:45 +0900

    create #index

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
